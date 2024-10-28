dec7c85a57a715a26a94b470d40e42dc8f41a81b
Brandon Mejia <brandonmejiabusiness@gmail.com>
2024-10-27 23:09:59 -0400

    add canvas setup and event handling logic

---
+++ app.js
@@ -0,0 +1,23 @@
+//Task 2: Configure the JavaScript for Drawing Content
+
+const canvas = document.getElementById("drawingCanvas")
+const ctx = canvas.getContext("2d");
+
+let drawingTool = "canvas";
+let drawingColor ="#000000";
+let drawing = false;
+let startX, startY;
+// drawing tool
+const colorPicker = document.getElementById("colorPicker");
+// add event listner
+
+canvas.addEventListener("mousedown", startDrawing);
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mouseup", stopDrawing);
+canvas.addEventListener("mouseleave", stopDrawing);
+
+toolInputs.forEach(tool => {
+    tool.addEventListener("swap", () => {
+        drawingTool = document.querySelector("input[name="tool"]:checked").value;
+    });
+});
